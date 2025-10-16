6bf3eaea11bae837280372d91113902bb6c1711d
Hashini Divyanjanee <143160465+HashiniDivyanjanee@users.noreply.github.com>
2025-10-16 21:04:47 +0530

    feat: add NavBar component and update App structure
    - Added a new NavBar component for navigation.
    - Updated App component to include NavBar and removed unused elements.
    - Added react-scripts dependency to package.json.
    - Commented out CSS rules in App.css and index.css for potential future use.

---
+++ src/components/navBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const navBar = () => {
+  return (
+    <header>
+      <div>Liyanage Tech Engineering</div>
+      <nav>
+        <a href="">Home</a>
+        <a href="">About Us</a>
+        <a href="">Shop</a>
+        <a href="">Contact</a>
+      </nav>
+    </header>
+  );
+};
+
+export default navBar;
