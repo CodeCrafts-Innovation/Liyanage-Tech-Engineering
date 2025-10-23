ff8d05de75bd6e4e1774b8546cb4578bf1951cff
Hashini Divyanjanee <143160465+HashiniDivyanjanee@users.noreply.github.com>
2025-10-23 15:55:00 +0530

    feat: update NavBar component with Bootstrap styling and remove unused CSS

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,15 +2,46 @@ import React from "react";
 
 const navBar = () => {
   return (
-    <header>
-      <div>Liyanage Tech Engineering</div>
-      <nav>
-        <a href="">Home</a>
-        <a href="">About Us</a>
-        <a href="">Shop</a>
-        <a href="">Contact</a>
-      </nav>
-    </header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-5">
+      <a class="navbar-brand" href="#">
+        Liyanage Tech Engineering
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="">
+              About Us
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="">
+              Shop
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
